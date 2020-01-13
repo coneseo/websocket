d60d0bc0d10c55dd405b41656df9dd5399e19fcc
coneseo <woon4754@gmail.com>
2020-01-13 23:50:48 +0900

    Initial commit

---
+++ src/main/java/scw/example/websocket/WebSocketHandler.java
@@ -0,0 +1,19 @@
+package scw.example.websocket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+  @Override
+  protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    String payload = message.getPayload();
+    log.info("payload {}",payload);
+    TextMessage textMessage = new TextMessage("welcome to my chat server!");
+    session.sendMessage(textMessage);
+  }
+}

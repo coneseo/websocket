6a226ffef0d19e06003b463453583ce0a25623f1
coneseo <woon4754@gmail.com>
2020-01-14 16:12:41 +0900

    Create ChatController
    채팅방의 생성 및 조회는 Rest api로 구현

---
+++ src/main/java/scw/example/websocket/ChatController.java
@@ -0,0 +1,26 @@
+package scw.example.websocket;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+  private final ChatService chatService;
+
+  @PostMapping
+  public ChatRoom createRoom(@RequestParam String name){
+    return chatService.createRoom(name);
+  }
+
+  @GetMapping
+  public List<ChatRoom> findAllRoom(){
+    return chatService.findAllRoom();
+  }
+}

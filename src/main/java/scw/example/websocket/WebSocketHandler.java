0d4b5486064ca4bf0d3a1c60e74c1ecf5452c82d
coneseo <woon4754@gmail.com>
2020-01-14 16:18:51 +0900

    Update WebSocketHandler
    추가한 로직에 맞게 수정

--- src/main/java/scw/example/websocket/WebSocketHandler.java
+++ src/main/java/scw/example/websocket/WebSocketHandler.java
@@ -1,5 +1,7 @@
 package scw.example.websocket;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -7,13 +9,19 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
+  private final ObjectMapper objectMapper;
+  private final ChatService chatService;
 
   @Override
   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
     String payload = message.getPayload();
     log.info("payload {}",payload);
-    TextMessage textMessage = new TextMessage("welcome to my chat server!");
-    session.sendMessage(textMessage);
+//    삭제 TextMessage textMessage = new TextMessage("welcome to my chat server!");
+//    삭제 session.sendMessage(textMessage);
+    ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+    ChatRoom chatRoom = chatService.findRoomById(chatMessage.getRoomId());
+    chatRoom.handleActions(session, chatMessage, chatService);
   }
 }

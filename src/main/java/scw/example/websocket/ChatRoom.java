b308a3aa862e704d9a5d7213f9a20d8d35eaefa7
coneseo <woon4754@gmail.com>
2020-01-15 19:01:10 +0900

    stomp를 적용한 후 chatroom dto 수정(session 관리 필요없어짐 -> pub/sub)

--- src/main/java/scw/example/websocket/ChatRoom.java
+++ src/main/java/scw/example/websocket/ChatRoom.java
@@ -2,6 +2,7 @@ package scw.example.websocket;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,24 +13,35 @@ import org.springframework.web.socket.WebSocketSession;
 public class ChatRoom {
   private String roomId;
   private String name;
-  private Set<WebSocketSession> sessions = new HashSet<>();
 
-  @Builder
-  public ChatRoom(String roomId, String name){
-    this.roomId = roomId;
-    this.name = name;
-  }
+  //stomp 방식
+//  private Set<WebSocketSession> sessions = new HashSet<>();
 
-  public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService){
-    if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
-      sessions.add(session);
-      chatMessage.setMessage(chatMessage.getSender()+"님이 입장하셨습니다.");
-    }
-    sendMessage(chatMessage, chatService);
-  }
+//  @Builder
+//  public ChatRoom(String roomId, String name){
+//    this.roomId = roomId;
+//    this.name = name;
+//  }
+//
+//  public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService){
+//    if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)){
+//      sessions.add(session);
+//      chatMessage.setMessage(chatMessage.getSender()+"님이 입장하셨습니다.");
+//    }
+//    sendMessage(chatMessage, chatService);
+//  }
+//
+//  private <T> void sendMessage(T message, ChatService chatService) {
+//    sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+//  }전
 
-  private <T> void sendMessage(T message, ChatService chatService) {
-    sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
+  //stomp 적용 후
+  //pub/sub 방식을 이용하면 국독자 관리가 알아서 되므로 웹소켓 세션관리가 필요없어진다.
+  public static ChatRoom create(String name){
+    ChatRoom chatRoom = new ChatRoom();
+    chatRoom.roomId = UUID.randomUUID().toString();
+    chatRoom.name = name;
+    return chatRoom;
   }
 
 }

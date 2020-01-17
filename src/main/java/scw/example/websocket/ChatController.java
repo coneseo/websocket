0bfab0c449434982d9466747f3a4cbd63fc6c13c
coneseo <woon4754@gmail.com>
2020-01-17 18:56:33 +0900

    ChatController 수정
    @MessageMapping을 통해 WebSocket으로 들어오는 메세지 발행을 처리한다. 클라이언트에서는 prefix를 붙여 /pub/chat/message 로 발행 요청을 하면 Controller가
    해당 메세지를 받아 처리한다. 메세지가 발행되면 /sub/chat/room/{roomId}로 메세지를 send하는 것을 볼 수 있는데 클라이언트에서는 해당 주소를 /sub/chat/room/{roomId} 구독
    하고 있다가 메세지가 전달되면 화면에 출력하면 됩니다. /sub/chat/room/{roomId}은 채팅방을 구분하기 때문에 pub/sub에서 Topic의 역할이라고 보면 된다.

--- src/main/java/scw/example/websocket/ChatController.java
+++ src/main/java/scw/example/websocket/ChatController.java
@@ -1,26 +1,20 @@
 package scw.example.websocket;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
-  private final ChatService chatService;
 
-  @PostMapping
-  public ChatRoom createRoom(@RequestParam String name){
-    return chatService.createRoom(name);
-  }
+  private final SimpMessageSendingOperations messagingTemplate;
 
-  @GetMapping
-  public List<ChatRoom> findAllRoom(){
-    return chatService.findAllRoom();
+  @MessageMapping("/chat/message")
+  public void message(ChatMessage message) {
+    if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+      message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+    messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
   }
 }

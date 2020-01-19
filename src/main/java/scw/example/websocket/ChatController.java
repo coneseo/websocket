f4769b814ac4daee63e2649ffcf1ca2b7dc44357
coneseo <woon4754@gmail.com>
2020-01-19 23:59:48 +0900

    입장 메세지 수정

--- src/main/java/scw/example/websocket/ChatController.java
+++ src/main/java/scw/example/websocket/ChatController.java
@@ -14,7 +14,7 @@ public class ChatController {
   @MessageMapping("/chat/message")
   public void message(ChatMessage message) {
     if (ChatMessage.MessageType.JOIN.equals(message.getType()))
-      message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+      message.setMessage(message.getSender() + "is entered");
     messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
   }
 }
